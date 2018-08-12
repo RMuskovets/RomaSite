53c8802f3bb9fe878c81c9c9af7c7888339e159e
RMuskovets <rmuskovets@gmail.com>
2018-08-12 15:24:15 +0300

    Add some basic HTML

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<?= 'Hello!' ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Roman Myskovets's Site</title>
+	<link rel="stylesheet" type="text/css" href="bulma.css">
+</head>
+<body>
+
+<div class="tabs is-centered">
+	<ul>
+		<li class="is-active"><a href="/">Home</a></li>
+		<li><a href="hangman.htm">Hangman!</a></li>
+	</ul>
+</div>
+
+</body>
+</html>
