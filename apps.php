7b8b1fbb86bfc0956711475d5b7be4912bc89727
RMuskovets <rmuskovets@gmail.com>
2018-08-12 21:46:04 +0300

    Update apps

--- apps.php
+++ apps.php
@@ -24,9 +24,77 @@
   </div>
 </div>
 <pre>
-      	<code class="python">
-      		<?= embed_github_gist('13f7c54a54d25918135c86e27469898a'); ?>
-      	</code>
-      </pre>
+  <code class="python">
+import random, time
+
+RD = 10**8
+
+def randDouble():
+  i = random.randint(0, RD)
+  return i / RD
+
+class Horse:
+  def __init__(self, name, c, s = 3, d = 0):
+    self.name, self.speed, self.ch, self.dist = name, s, c, d
+
+  def move(self):
+    self.dist += randDouble() * self.speed
+
+  def print(self):
+    print (('.' * int(self.dist)) + self.ch)
+
+class Hippodrome:
+  game = None
+
+  def __init__(self, listH):
+    self.horses = listH
+
+  def move(self):
+    for h in self.horses:
+      h.move()
+
+  def print(self):
+    for h in self.horses:
+      h.print()
+    for i in range(0, 10):
+      print()
+
+  def run(self):
+    for i in range(100):
+      self.move()
+      self.print()
+      time.sleep(.2)
+
+  @property
+  def winner(self):
+    max_val = max([x.dist for x in self.horses])
+    for h in self.horses:
+      if max_val == h.dist:
+        return h
+
+  @property
+  def looser(self):
+    max_val = min([x.dist for x in self.horses])
+    for h in self.horses:
+      if max_val == h.dist:
+        return h
+END_INPUT = 'quit'
+def main():
+  print ('Enter data of horses in format "name$speed$raceChar". To reach end, type "%s"' % END_INPUT)
+  horses = []
+  while 1:
+    s = input()
+    name = s.split('$')[0]
+    speed = int(s.split('$')[1])
+    rc = s.split('$')[2][0]
+    horses.append(Horse(name, rc, s=speed))
+  Hippodrome.game = Hippodrome(horses)
+  Hippodrome.game.run()
+  print ('Winner is %s!' % Hippodrome.game.winner.name)
+  print ('Looser is %s!' % Hippodrome.game.looser.name)
+if __name__ == '__main__':
+  main()
+  </code>
+</pre>
 </body>
 </html>
